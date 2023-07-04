c212870c67c9df65915744e2ffd22de9e0455c0b
zispidd <zispidd@gmail.com>
2023-07-04 21:19:10 +0200

    feat: add link

---
+++ lib/animations/Link.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+interface IProps {
+    style?: StyleProp<ViewStyle>;
+}
+declare const Link: React.FC<IProps>;
+export default Link;
